7d90dbc2330b1fd7b32926ad4bf4ff71760047a8
Joseph Shook <JoeShook@Gmail.com>
2024-09-29 08:07:58 -0700

    wip Cert Utilites

--- Shared/Components/CertificatePKIViewer.razor.cs
+++ Shared/Components/CertificatePKIViewer.razor.cs
@@ -6,12 +6,6 @@ namespace UdapEd.Shared.Components;
 
 public partial class CertificatePKIViewer : ComponentBase
 {
-    /// <summary>
-    /// A X509Certificate as a Base64 encoding of the DER representation
-    /// </summary>
-    [Parameter]
-    public string? Certificate { get; set; }
-
     [Parameter] public string? JwtHeaderWithx5c { get; set; }
     [Inject] private IDiscoveryService MetadataService { get; set; } = null!;
     [Inject] private IInfrastructure Infrastructure { get; set; } = null!;
